1252047c2b6a2e698c29bc970a1f465fde30a014
shkim <skok1025@naver.com>
2023-12-19 12:57:19 +0900

    pydandic 적용하여 요청데이터 유효성 체크 - Path 파라미터 유효성 검증

--- book2.py
+++ book2.py
@@ -1,9 +1,8 @@
 from typing import Optional
 
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
-
 import uvicorn
+from fastapi import FastAPI, Path
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -14,13 +13,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date: int):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -29,6 +30,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
 
     # Example Value 값을 아래 Config 에서 지정해줄 수 있습니다.
     class Config:
@@ -38,23 +40,53 @@ class BookRequest(BaseModel):
                 "author": "Kim",
                 "description": "Description 1234",
                 "rating": 3,
+                "published_date": 2023
             }
         }
 
 
 BOOKS = [
-    Book(1, "Title One", "Author One", "Description One", 5),
-    Book(2, "Title Two", "Author Two", "Description Two", 4),
-    Book(3, "Title Three", "Author Three", "Description Three", 3),
-    Book(4, "Title Four", "Author Four", "Description Four", 2),
-    Book(5, "Title Five", "Author Five", "Description Five", 1),
-    Book(6, "Title Six", "Author Six", "Description Six", 0),
+    Book(1, "Title One", "Author One", "Description One", 5, 2019),
+    Book(2, "Title Two", "Author Two", "Description Two", 3, 2020),
+    Book(3, "Title Three", "Author Three", "Description Three", 3, 2019),
+    Book(4, "Title Four", "Author Four", "Description Four", 2, 2019),
+    Book(5, "Title Five", "Author Five", "Description Five", 1, 2019),
+    Book(6, "Title Six", "Author Six", "Description Six", 0, 2023),
 ]
 
 
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int = Path(gt=0, le=len(BOOKS))):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int):
+    books_return = []
+
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_return.append(book)
+
+    return books_return
+
+
+@app.get("/books-by-date/")
+async def read_book_publish_date(publish_date: int):
+    books_return = []
+
+    for book in BOOKS:
+        if book.published_date == publish_date:
+            books_return.append(book)
+
+    return books_return
 
 
 @app.post("/create-book")
@@ -69,6 +101,24 @@ def find_book_id(book: Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
 
     return book
+
+
+@app.put("/books/update_book")
+async def update_book(book_request: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_request.id:
+            BOOKS[i] = book_request
+
+            return BOOKS[i]
+
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int = Path(gt=0, le=len(BOOKS))):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
+
 
 
 if __name__ == "__main__":
