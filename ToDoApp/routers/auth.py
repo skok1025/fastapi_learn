6b8948bb312d545ccf6ee9f9a45643ac8b843726
shkim30 <shkim30@cafe24corp.com>
2024-03-01 17:16:02 +0900

    jwt 학습

--- ToDoApp/routers/auth.py
+++ ToDoApp/routers/auth.py
@@ -23,7 +23,7 @@ ALGORITHM = "HS256"
 
 # pip install passlib[bcrypt]
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 
 class CreateUserRequest(BaseModel):
@@ -101,7 +101,10 @@ async def login_for_access_token(
     # pip install python-multipart
     user = authenticate_user(form.username, form.password, db)
     if not user:
-        return "Failed Authentication"
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+        )
 
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
     return {"access_token": token, "token_type": "bearer"}
